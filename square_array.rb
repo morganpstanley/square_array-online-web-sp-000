18e324be4ab16139222e4e0cb09f7df6fa1c9cca
Morgan Stanley <morganpstanley@gmail.com>
2019-07-02 21:31:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  # your code here
+  new_numbers = []
   array.each do |number|
-  number *= number
-  new_numbers[] = number
-end
+    number *= number
+    new_numbers.push(number)
+  end
+  return new_numbers
 end
